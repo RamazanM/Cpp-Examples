74e48d937874e3b9fc34d1081e14fe4639503069
RamazanM <r.ramazanmutlu@gmail.com>
2016-12-03 13:27:13 +0300

    İlk app

---
+++ HelloWorld.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout<<"Hello World\n";
+//---------------------------------------------------------
+  string mesaj="Merhaba dunya";
+  cout<<mesaj<<endl;
+//---------------------------------------------------------
+  int sayi=12;
+  cout<<sayi<<" bir sayi ve "<<mesaj<<" bir string"<<endl;
+//---------------------------------------------------------
+  cout<<"Arka arkaya iki sayı giriniz:";
+  int s1,s2;
+  cin>>s1>>s2;
+  cout<<"toplamı:"<<s1+s2;
+//---------------------------------------------------------
+  return 0;
+}
