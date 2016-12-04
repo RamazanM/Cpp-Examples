6bb81eb6ed0d67d856568871a1e4110950a96f75
RamazanM <r.ramazanmutlu@gmail.com>
2016-12-04 11:53:52 +0300

    Update

--- struct_typedef.cpp
+++ struct_typedef.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 struct Member{
@@ -7,20 +8,27 @@ struct Member{
   string isim;
   int yas;
 };
-
 typedef Member uye;
 
+int uye_sayisi=3;
+
+void uyeYazdir(uye gelen);
+
 int main() {
-  uye uyeler[5];
-  for(int i=0 ; i<5 ; i++){
+  uye uyeler[uye_sayisi];
+  for(int i=0 ; i<uye_sayisi ; i++){
     uyeler[i].id=i+1;
     cout<<i+1<<". üyenin adını giriniz:";
     cin>>uyeler[i].isim;
     cout<<i+1<<". üyenin yaşını giriniz:";
     cin>>uyeler[i].yas;
   }
-  for(int i=0 ; i<4 ; i++){
-    cout<<"Üye "<<i+1<<"'in"<<endl<<" Id'si:"<<uyeler[i].id<<", İsmi:"<<uyeler[i].isim<<", Yaşı:"<<uyeler[i].yas<<endl;
+  for(int i=0 ; i<uye_sayisi ; i++){
+    uyeYazdir(uyeler[i]);
   }
   return 0;
+}
+
+void uyeYazdir(uye gelen){
+  cout<<"Üyenin"<<endl<<" Id'si:"<<gelen.id<<", İsmi:"<<gelen.isim<<", Yaşı:"<<gelen.yas<<endl;
 }
